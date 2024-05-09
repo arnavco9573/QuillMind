6e80b0e5b6f38c261c77b753f223d32a32dcb434
arnavco9573 <arnavsharm589@gmail.com>
2024-05-09 19:28:44 +0530

    sending

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,7 +4,7 @@ import User from "@models/users";
 import { connectToDb } from "@utils/database";
 
 const handler = NextAuth({
-  providers: [
+  providers: [ 
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
