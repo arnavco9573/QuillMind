9baf4c81b3c66105d2eb9efabbc85f91f0c234ec
arnavco9573 <arnavsharm589@gmail.com>
2025-03-03 23:06:48 +0530

    final

--- utils/database.js
+++ utils/database.js
@@ -2,23 +2,22 @@ import mongoose from "mongoose";
 
 let isConnected = false;
 
-export const connectToDb = async ()=>{
-    mongoose.set('strictQuery' ,true);
-    if(isConnected){
-        console.log("MongoDb is connected");
+export const connectToDb = async () => {
+    mongoose.set("strictQuery", true);
+
+    if (isConnected) {
+        console.log("MongoDB is already connected");
         return;
     }
 
     try {
-        await mongoose.connect(process.env.MONGODB_URI,{
-            dbName:"share_prompt",
-            useNewUrlParser:true,
-            useUnifiedTopology:true,
-        })
-        isConnected= true;
-        console.log("MongoDb successfully Connected")
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: "share_prompt",
+        });
+
+        isConnected = true;
+        console.log("MongoDB successfully connected");
     } catch (error) {
-        console.log(error);
-        
+        console.error("MongoDB connection error:", error);
     }
-}
+};
